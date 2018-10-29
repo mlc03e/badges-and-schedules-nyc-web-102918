e7a449ae4f7e4b51e75cbb4224300a8dc31bc6f0
Meghan Campbell <mlc03e@gmail.com>
2018-10-29 01:15:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  name.each do |person|
+    puts "Hello, my name is #{person}."
+  end
+end
+  
+def batch_badge_creator(arr)
+ r = []
+ arr.each do |name|
+   puts "Hello, my name is #{name}."
+ (r)<< arr
+ return r
+end
+end
